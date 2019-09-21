3db8b10793f2fdb5010c492fdd842e2058aa2f77
fgxut <fgxut1206@gmail.com>
2019-09-21 23:15:04 +0900

    モデルスペックを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a user_name, account_name, email, and password" do
+    user = User.new(
+      user_name: "test",
+      account_name: "test",
+      email: "test@example.com",
+      password: "hogehoge",
+    )
+    expect(user).to be_valid
+  end
 end
