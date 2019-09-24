294270cf6c5f29f944e5c330be93e6099c0b12e2
fgxut <fgxut1206@gmail.com>
2019-09-24 23:47:34 +0900

    [update]フラッシュメッセージを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do 
   get 'users/new'
   root 'static_pages#top'
-  get '/top', to:'static_pages#top'
-  get '/home', to:'static_pages#home'
-  get '/help', to:'static_pages#help'
-  get '/about', to:'static_pages#about'
-  get '/contact', to:'static_pages#contact'
-  get '/signup', to:'users#new'
+  get '/top', to: 'static_pages#top'
+  get '/home', to: 'static_pages#home'
+  get '/help', to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
+  get '/signup', to: 'users#new'
+  post '/signup',  to: 'users#create'
   resources :users
 end
