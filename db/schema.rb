ee884fb5f61ff6296b2fa5f1318a05d8bc8c08f9
fgxut <fgxut1206@gmail.com>
2019-09-27 00:44:12 +0900

    [add]remember me 機能を追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_19_125219) do
+ActiveRecord::Schema.define(version: 2019_09_26_135340) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "user_name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2019_09_19_125219) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.string "remember_digest"
     t.index ["account_name"], name: "index_users_on_account_name", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
   end
