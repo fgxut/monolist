733db5ddff5af6e7723eae0eab2baaa38b408c4f
fgxut <fgxut1206@gmail.com>
2019-10-02 23:37:10 +0900

    [add]持ち物の削除機能を追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :logged_in_user, only: [:new, :create, :destroy]
+  before_action :correct_user,   only: :destroy
 
   def new
     @item = Item.new
@@ -17,11 +18,19 @@ class ItemsController < ApplicationController
   end
 
   def destroy
+    @item.destroy
+    flash[:success] = "持ち物は削除されました"
+    redirect_to request.referrer || root_url
   end
 
   private
 
     def item_params
       params.require(:item).permit(:content)
+    end
+
+    def correct_user
+      @item = current_user.items.find_by(id: params[:id])
+      redirect_to root_url if @item.nil?
     end
 end
