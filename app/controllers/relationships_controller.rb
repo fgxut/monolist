5cbf4920370a68c5ede107bf6922ef099764bb9e
fgxut <fgxut1206@gmail.com>
2019-10-06 22:59:52 +0900

    [update]日本語に修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,6 +5,7 @@ class RelationshipsController < ApplicationController
     user = User.find(params[:followed_id])
     current_user.follow(user)
     redirect_to user
+    debugger
   end
 
   def destroy
