251f6216984716e4265f210940b4dca85bbb3f1b
fgxut <fgxut1206@gmail.com>
2019-10-15 20:18:43 +0900

    [update]gravatarをユーザー画像に修正

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -4,4 +4,8 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.user_name, class: "gravatar")
   end
+
+  def user_initial_picture(user, width: 100, height: 100)
+    image_tag("user_initial.png", width: "#{width}", height: "#{height}")
+  end
 end
